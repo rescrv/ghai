619eaba0f11224c5337c564a51c0d6dc3ac9f78b
Robert Escriva <robert@rescrv.net>
2024-12-22 15:36:08 -0800

    print jobs urls

---
+++ src/bin/ghai-print-jobs-urls.rs
@@ -0,0 +1,40 @@
+use arrrg::CommandLine;
+
+use ghai::Action;
+
+#[derive(Debug, Default, Eq, PartialEq, arrrg_derive::CommandLine)]
+struct Options {
+    #[arrrg(required, "The owner of the repository")]
+    owner: String,
+    #[arrrg(required, "The name of the repository")]
+    repo: String,
+    #[arrrg(optional, "Filter to a given actor's actions")]
+    actor: Option<String>,
+    #[arrrg(optional, "Filter to a given status")]
+    status: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let (options, free) =
+        Options::from_command_line_relaxed("USAGE: ghai-notify-finished-action [options]");
+    if !free.is_empty() {
+        eprintln!("command takes no arguments");
+        std::process::exit(1);
+    }
+    for action in Action::fetch_all(
+        options.owner.clone(),
+        options.repo.clone(),
+        options.actor.clone(),
+        None,
+        None,
+        options.status.clone(),
+        None,
+        None,
+    )?
+    .into_iter()
+    {
+        println!("{}", action.jobs_url);
+    }
+    Ok(())
+}
